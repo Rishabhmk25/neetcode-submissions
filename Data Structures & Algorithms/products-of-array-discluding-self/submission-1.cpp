65b9394676e1f3ac905f2a61cce6f9a843c1a040
Rishabh Khadse <rishabhmk25@gmail.com>
2026-08-09 16:37:09 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> p(n), s(n);
+        p[0] = 1;
+        s[n-1] = 1;
+        for(int i = 1; i < n; i++) {
+            p[i] = p[i-1]*nums[i-1];
+            s[n-i-1] = s[n-i]*nums[n-i];
+        }
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++) ans[i] = p[i]*s[i];
+        return ans;
+    }
+};
